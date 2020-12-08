48040d9491f3a777c9e70f04f46765977ffabde0
Cody Carse <cody.carse@upstart.com>
2020-12-08 14:12:53 -0500

    Update deps

--- src/3-test.ts
+++ src/3-test.ts
@@ -1,7 +1,7 @@
 import * as puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false}); // Can pass in options here, eg {headless: false}
+  const browser = await puppeteer.launch({ headless: false }); // Can pass in options here, eg {headless: false}
   const page = await browser.newPage();
 
   // Load the page
@@ -18,17 +18,16 @@ import * as puppeteer from 'puppeteer';
   // Click the login button and wait for the page to load.
   const loginButtonSelector = 'form > fieldset > div:nth-child(3) > button';
   await Promise.all([
-    page.waitForNavigation({waitUntil: 'networkidle0'}),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
     page.click(loginButtonSelector),
   ]);
 
   // Check if the tree has nodes. 
   const treeSelector = '#app > div > div.container-fluid > div > div.col-lg-5 > div:nth-child(2) > div > ul';
   const hasNodes = await page.evaluate(`document.querySelector('${treeSelector}').children.length > 0`);
-  
+
   // If this were a real test this is where you'd probably use your testing library to assert that the tree is populated.
   console.log(`Tree is populated: ${hasNodes ? 'Yes' : 'No'}`);
 
-  await page.waitFor(2000);
   await browser.close();
 })();
